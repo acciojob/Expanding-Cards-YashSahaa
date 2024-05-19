106b2c9d29520eb428ddb3fd2eeb6fbf823a0a4f
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-19 17:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const panels = document.getElementsByClassName("panel")
+const imgtext = document.getElementsByClassName("img-text")
+let curr = 0
+for (let i = 0; i < panels.length; i++) {
+	panels[i].addEventListener("click",()=>{
+		panels[curr].classList.remove("active")
+		panels[i].classList.add("active")
+        imgtext[i].classList.remove("disp")
+		imgtext[curr].classList.add("disp")
+		curr = i
+	})
+}
